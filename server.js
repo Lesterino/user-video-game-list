4e4680293de146791ec696e6b752b31f14e9987d
lesterino <lesterleongjr@gmail.com>
2021-07-30 19:17:47 -0700

    cleans server.js

--- server.js
+++ server.js
@@ -19,20 +19,14 @@ app.use(require('./config/checkToken'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/games', require('./routes/api/games'));
 
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
 
 app.listen(port, function() {
   console.log(`Express app running on port ${port}`)
 });
 
-// app.use(express.static(path.join(__dirname, 'build')));
-
 // Put API routes here, before the "catch all" route
 
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
